9e902f2d947ac1d73f9be21de6392829a8a2bfa5
PasswordProtected <61480560+PasswordProtected@users.noreply.github.com>
2021-12-18 22:28:12 +1100

    slapping command

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 import random
-#import psycopg2
+from discord import user
+import psycopg2
+from psycopg2 import Error
 
 import discord
 from discord.ext import commands
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 bot = commands.Bot(command_prefix = '$')
 
 @bot.event
@@ -26,6 +27,23 @@ async def hello(ctx):
 
     response = random.choice(greetings)
     await ctx.send(response)
+
+@bot.command(name = 'slap', help = 'Slaps the person whom you specify.')
+async def slap(ctx, user, *, reason):
+    slap_gifs = [
+        'https://tenor.com/view/nope-stupid-slap-in-the-face-phone-gif-15151334',
+        'https://tenor.com/view/baka-slap-huh-angry-gif-15696850',
+        'https://tenor.com/view/pikachu-slap-fight-mad-no-gif-16415016',
+        'https://tenor.com/view/slap-cat-gif-11314821',
+        'https://tenor.com/view/face-slap-gif-18146312',
+        'https://tenor.com/view/spank-slap-butt-anime-gif-17784858',
+
+    ]
+    #await ctx.message.delete()
+    await ctx.send('{0.author.mention} slapped {1} {2}'.format(ctx, user, reason))
+    await ctx.send(random.choice(slap_gifs))
+
+
 
 
 bot.run(TOKEN)
