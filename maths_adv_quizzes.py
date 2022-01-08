7090f50b1eedacc60384ce5c87d1077c441e3adc
PasswordProtected <61480560+PasswordProtected@users.noreply.github.com>
2022-01-09 09:59:53 +1100

    directory changes for image files

--- maths_adv_quizzes.py
+++ maths_adv_quizzes.py
@@ -1,6 +1,6 @@
 questions = {
-    'images\maths_adv_quizzes\Q1.png': '2, 28, 4',
-    'images\maths_adv_quizzes\Q2.png': '225',
-    'images\maths_adv_quizzes\Q3.png': '1250, 50*25',
-    'images\maths_adv_quizzes\Q4.png': '8*20',
+    '/images/maths_adv_quizzes/Q1.png': '2, 28, 4',
+    '/images/maths_adv_quizzes/Q2.png': '225',
+    '/images/maths_adv_quizzes/Q3.png': '1250, 50*25',
+    '/images/maths_adv_quizzes/Q4.png': '8*20',
 }
