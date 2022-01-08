e8f2d158abc6b79a62035e90bccaaa0f56e8e46d
PasswordProtected <61480560+PasswordProtected@users.noreply.github.com>
2022-01-09 10:38:44 +1100

    subfolder 'maths_adv_quizzes' deleted and transferred images in subfolder to folder 'images'

--- maths_adv_quizzes.py
+++ maths_adv_quizzes.py
@@ -1,6 +1,6 @@
 questions = {
-    './images/maths_adv_quizzes/Q1.png': '2, 28, 4',
-    './images/maths_adv_quizzes/Q2.png': '225',
-    './images/maths_adv_quizzes/Q3.png': '1250, 50*25',
-    './images/maths_adv_quizzes/Q4.png': '8*20',
+    'Q1.png': '2, 28, 4',
+    'Q2.png': '225',
+    'Q3.png': '1250, 50*25',
+    'Q4.png': '8*20',
 }
